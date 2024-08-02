b6ec46f43fea9e737cac049cb148760a9c8da71a
ondrahladik <hladik.ondra@seznam.cz>
2024-08-03 00:14:03 +0200

    Edit

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('data.json')
+    fetch('json/data.json')
         .then(response => response.json())
         .then(jsonData => {
             const results = document.getElementById('results');
