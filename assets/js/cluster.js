6e9874c81fe226c4497a3b94af1a770bf78a902e
ondrahladik <ondra@ok1kky.cz>
2026-05-08 12:13:24 +0200

    Add QO-100 mode styling and detection
    Add a new CSS class .mode-qo100 to style QO-100 mode markers and include 'QO-100' in the mode extraction list so messages mentioning QO-100 are recognized. Changes touch assets/css/cluster.css and assets/js/cluster.js.

--- assets/js/cluster.js
+++ assets/js/cluster.js
@@ -66,7 +66,7 @@ function freqBandClass(khz) {
 }
 
 function extractMode(msg) {
-  const modes = ['FT8','FT4','JT65','JT9','CW','SSB','AM','FM','RTTY','PSK31','PSK','WSPR','JS8'];
+  const modes = ['FT8','FT4','JT65','JT9','CW','SSB','AM','FM','RTTY','PSK31','PSK','WSPR','JS8','QO-100'];
   for (const m of modes) {
     if (new RegExp('\\b' + m + '\\b', 'i').test(msg)) return m;
   }
